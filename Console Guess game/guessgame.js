a1ada0b8cba128313fd3144ce0f2dd37cff8fb43
Tomasz Rohda <tomaszrohda@wp.pl>
2022-03-31 11:56:48 +0200

    Modify checkResult function, add stratRound function and move consol logs to functions

--- Console Guess game/guessgame.js
+++ Console Guess game/guessgame.js
@@ -1,23 +1,41 @@
 // alert('We`re about to start simply "number guessing" console game');
 // alert('I`ve chosen one number which is more than 0 and less than 11');
 let roundNumber = 1;
-let playerNumber = parseInt(prompt('Write below your "shot" from 1 to 20, and open console in DevTools'));
-
 const randomGenerateNumber =  Math.ceil(Math.random()*10);
-console.log(randomGenerateNumber);
-console.log(`It's Yours ${roundNumber} of three chances.`)
-console.log(`Your number is:  ${playerNumber}.`);
-console.log(`Now im thinking... 🤔🤔🤔`)
+let playerNumber = 0;
 
-
-const checkResult = (randNum, pickedNum, turn) =>{
-    if(randNum === pickedNum){
+const checkResult = () =>{
+    if (roundNumber === 3){
+        console.warn("You lose! 😥");
+    
+        // return 0;
+    }
+    else if(randomGenerateNumber === playerNumber){
         console.warn(`Congratulation. You WIN 🥇`);
-    }else if (randNum > pickedNum){
-        turn++;
-        console.warn(`Your number is too small ⬇️`)
+
+    }else if (randomGenerateNumber > playerNumber){
+        console.warn('Your number is "too small"⬇️');
+        roundNumber++;
+        startRound(roundNumber);
+        setTimeout(checkResult, 2000);
+    }else if (randomGenerateNumber < playerNumber){
+        console.warn('Your number is "too big"⬆️');
+        roundNumber++;
+        startRound(roundNumber);
+        setTimeout(checkResult, 2000);
     }
 }
 
-setTimeout (checkResult(randomGenerateNumber, playerNumber,), 5000);
+let startRound = (turn) =>{
+    playerNumber = parseInt(prompt('Write below your "shot":'));
+    console.log(randomGenerateNumber);
+    console.log(`It's Yours ${roundNumber} of three chances.`)
+    console.log(`Your number is:  ${playerNumber}.`);
+    console.log(`Now im thinking... 🤔🤔🤔`);
+    timer2 = setTimeout(checkResult, 2000);
+    
+}
+
+startRound(roundNumber);
+
 

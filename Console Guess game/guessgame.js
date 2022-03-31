dc009b4e4a3ec634e7986afc04e6ad228f7feda9
Tomasz Rohda <tomaszrohda@wp.pl>
2022-03-31 12:42:35 +0200

    Make little correct in scpript, assigning timers to variables and add background to body.

--- Console Guess game/guessgame.js
+++ Console Guess game/guessgame.js
@@ -1,41 +1,45 @@
-// alert('We`re about to start simply "number guessing" console game');
-// alert('I`ve chosen one number which is more than 0 and less than 11');
+alert('We`re about to start simply "number guessing" console game');
+alert('I`ve chosen one number which is more than 0 and less than 11');
 let roundNumber = 1;
 const randomGenerateNumber =  Math.ceil(Math.random()*10);
 let playerNumber = 0;
+let timer1 = ''; 
+let timer2 = ''; 
+let timer3 = ''; 
 
 const checkResult = () =>{
-    if (roundNumber === 3){
-        console.warn("You lose! 😥");
-    
-        // return 0;
-    }
-    else if(randomGenerateNumber === playerNumber){
-        console.warn(`Congratulation. You WIN 🥇`);
-
+    if (randomGenerateNumber === playerNumber){
+        setTimeout(()=>{console.warn(`Congratulation. You WIN!🥇`)}, 2000);
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+        clearTimeout(timer3);
+    }else if(roundNumber === 3){
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+        clearTimeout(timer3);
+        setTimeout(()=>{console.warn("You lose! 😥")},2000);  
     }else if (randomGenerateNumber > playerNumber){
+        console.log("Bad Shot!📦");
         console.warn('Your number is "too small"⬇️');
         roundNumber++;
         startRound(roundNumber);
-        setTimeout(checkResult, 2000);
+        timer1 = setTimeout(checkResult, 2000);
     }else if (randomGenerateNumber < playerNumber){
+        console.log("Bad Shot!📦");
         console.warn('Your number is "too big"⬆️');
         roundNumber++;
         startRound(roundNumber);
-        setTimeout(checkResult, 2000);
+        timer2 = setTimeout(checkResult, 2000);
     }
 }
 
 let startRound = (turn) =>{
     playerNumber = parseInt(prompt('Write below your "shot":'));
     console.log(randomGenerateNumber);
-    console.log(`It's Yours ${roundNumber} of three chances.`)
+    console.log(`It's Yours ${turn} of three chances.`)
     console.log(`Your number is:  ${playerNumber}.`);
     console.log(`Now im thinking... 🤔🤔🤔`);
-    timer2 = setTimeout(checkResult, 2000);
-    
+    timer3 = setTimeout(checkResult, 2000);
 }
 
 startRound(roundNumber);
-
-
